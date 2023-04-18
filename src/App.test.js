e83ece1648347b37a514d89a083ebad504f40236
Roshni Singh <roshni.rx@gmail.com>
2023-04-18 22:11:27 +0530

    budget app testing

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const appElement = screen.getByText(/Planner/);
-  expect(appElement.textContent).toEqual('Planner');
+  const appElement = screen.getByText(/My Budget Planner/);
+  expect(appElement.textContent).toEqual('My Budget Planner');
 });
