f1fb2fb4cc35ab5279ba5262359a473d25a86f22
Roshni Singh <roshni.rx@gmail.com>
2023-04-20 11:16:35 +0530

    test folder

--- src/__tests__/AddExpenseForm.test.js
+++ src/__tests__/AddExpenseForm.test.js
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import AddExpenseForm from '../AddExpenseForm';
+import AddExpenseForm from '../components/AddExpenseForm';
 
 test('typing into input and displaying todo', () => {
     render(<AddExpenseForm/>);
